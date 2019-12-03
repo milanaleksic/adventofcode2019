85118b589e8810e4e12d2f202ea69ee7f245fd7a
Milan Aleksic <milanaleksic@gmail.com>
2019-12-03 13:21:50 +0100

    day 3: solution b

--- src/advent03/lib.rs
+++ src/advent03/lib.rs
@@ -1,56 +1,142 @@
+use std::cmp::Eq;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 
 use crate::common::read_input_as_rows_strings;
 
 use super::common;
+use std::fmt::{Display, Formatter, Error, Debug};
+
+struct Cell {
+    x: i32,
+    y: i32,
+    latency: i32,
+}
+
+impl Cell {
+    fn new(x: i32, y: i32, latency: i32) -> Cell {
+        Cell {
+            x,
+            y,
+            latency,
+        }
+    }
+    fn distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+    fn latency(&self) -> i32 {
+        self.latency
+    }
+}
+
+impl Hash for Cell {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Cell {}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
+impl Debug for Cell {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
 
 pub struct Solver {}
 
 impl Solver {
-    fn solve_for(&self, cable1: Vec<String>, cable2: Vec<String>) -> i32 {
+    fn get_data(&self) -> Vec<Vec<String>> {
+        let cables: Vec<Vec<String>> = read_input_as_rows_strings("advent03/input.txt")
+            .iter()
+            .map(|x| x.split(',').map(|x| x.to_owned()).collect())
+            .collect();
+        cables
+    }
+
+    fn solve_for(&self, cable1: Vec<String>, cable2: Vec<String>) -> (i32, i32) {
         let cable1 = self.parse_road(cable1);
         let cable2 = self.parse_road(cable2);
         let field1 = self.walk(cable1);
+//        println!("Field1={:?}", field1);
         let field2 = self.walk(cable2);
-        let mut best_match: Option<i32> = None;
-        for (slot, _) in field1 {
-            if let Some(_) = field2.get(&slot) {
-                let new = slot.0.abs() + slot.1.abs();
-                best_match = match best_match {
+        let mut best_match_distance: Option<i32> = None;
+        let mut best_match_latency: Option<i32> = None;
+        for (cell1, _) in field1 {
+            if let Some(latency2) = field2.get(&cell1) {
+                let new_distance = cell1.distance();
+                best_match_distance = match best_match_distance {
                     Some(m) => {
-                        if new < m {
-                            Some(new)
+                        if new_distance < m {
+                            Some(new_distance)
                         } else {
-                            best_match
+                            best_match_distance
                         }
                     }
-                    None => Some(new),
+                    None => Some(new_distance),
+                };
+                let new_latency = cell1.latency() + *latency2;
+                best_match_latency = match best_match_latency {
+                    Some(m) => {
+                        if new_latency < m {
+                            Some(new_latency)
+                        } else {
+                            best_match_latency
+                        }
+                    }
+                    None => Some(new_latency),
                 };
             }
         }
-        best_match.unwrap()
+        (best_match_distance.unwrap(), best_match_latency.unwrap())
     }
 
-    fn walk(&self, cable: Vec<(i32, i32)>) -> HashMap<(i32, i32), bool> {
+    fn walk(&self, cable: Vec<(i32, i32)>) -> HashMap<Cell, i32> {
         let mut iter = (0, 0);
-        let mut field: HashMap<(i32, i32), bool> = HashMap::new();
+        let mut latency = 0;
+        let mut field: HashMap<Cell, i32> = HashMap::new();
         for step in cable {
             if step.0 != 0 {
-                let (begin, end) = if step.0 < 0 { (step.0, 0) } else { (0, step.0) };
+                let begin = iter.0;
+                let end = begin + step.0.abs();
+                let incr = if step.0 < 0 { -1 } else { 1 };
                 for ix in begin..end {
-                    field.insert((iter.0 + ix, iter.1), true);
+                    latency += 1;
+                    let cell = Cell::new(iter.0 + (ix-begin+1)*incr, iter.1, latency);
+                    if !field.contains_key(&cell) {
+                        field.insert(cell, latency);
+                    }
                 }
                 iter.0 += step.0;
             }
             if step.1 != 0 {
-                let (begin, end) = if step.1 < 0 { (step.1, 0) } else { (0, step.1) };
+                let begin = iter.1;
+                let end = begin + step.1.abs();
+                let incr = if step.1 < 0 { -1 } else { 1 };
                 for iy in begin..end {
-                    field.insert((iter.0, iter.1 + iy), true);
+                    latency += 1;
+                    let cell = Cell::new(iter.0, iter.1 + (iy-begin+1)*incr, latency);
+                    if !field.contains_key(&cell) {
+                        field.insert(cell, latency);
+                    }
                 }
                 iter.1 += step.1;
             }
         }
-        field.remove(&(0, 0));
+        field.remove(&Cell::new(0, 0, 0));
         field
     }
 
@@ -75,17 +161,16 @@ impl common::Solver for Solver {
     }
 
     fn solve_a(&self) -> String {
-        let cables: Vec<Vec<String>> = read_input_as_rows_strings("advent03/input.txt")
-            .iter()
-            .map(|x| x.split(',').map(|x| x.to_owned()).collect())
-            .collect();
-        format!("{}", self.solve_for(cables[0].clone(), cables[1].clone()))
+        let cables = self.get_data();
+        format!("{}", self.solve_for(cables[0].clone(), cables[1].clone()).0)
     }
 
     fn solve_b(&self) -> String {
-        unimplemented!("Later on")
+        let cables = self.get_data();
+        format!("{}", self.solve_for(cables[0].clone(), cables[1].clone()).1)
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -99,7 +184,7 @@ mod tests {
     fn test0() {
         assert_eq!(
             Solver {}.solve_for(vec_of_strings!["R8", "U5", "L5", "D3"], vec_of_strings!["U7", "R6", "D4", "L4"]),
-            6
+            (6, 30)
         );
     }
 
@@ -110,7 +195,7 @@ mod tests {
                 vec_of_strings!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
                 vec_of_strings!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"],
             ),
-            159
+            (159, 610)
         );
     }
 
@@ -121,7 +206,7 @@ mod tests {
                 vec_of_strings!["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
                 vec_of_strings!["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"],
             ),
-            135
+            (135, 410)
         );
     }
 }
